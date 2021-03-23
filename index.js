5361a853539f9ec379925b82f00d4d53015e0e45
Jesus <gsusss2004@gmail.com>
2021-03-23 18:47:01 +0000

    intent main

--- index.js
+++ index.js
@@ -7,13 +7,10 @@ const app = actionssdk();
 const optionsNeedA = new Set();
 optionsNeedA.add('horse').add('phone');
 
-app.handle('greeting', conv => {
-  let message = 'A wondrous greeting, adventurer! Welcome back to the mythical land of Gryffinberg!';
-  /*if (!conv.user.lastSeenTime) {
-    message = 'Welcome to the mythical land of  Gryffinberg! Based on your clothes, you are not from around these lands. It looks like you\'re on your way to an epic journey.';
-  }*/
-  conv.add(message);
+app.intent('actions.intent.MAIN', conv => {
+  conv.ask('Hi, how is it going MAIN camp?')
 });
+
 app.intent('actions.intent.greeting', conv => {
   conv.ask('A wondrous greeting, adventurer! Welcome back to the mythical land of Gryffinberg!');
 })
