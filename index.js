8ad6033689a30e0ba5208de235bf95a8105d11f4
Jesus <gsusss2004@gmail.com>
2021-03-23 18:29:40 +0000

    fulfilment simplified

--- index.js
+++ index.js
@@ -7,20 +7,20 @@ optionsNeedA.add('horse').add('phone');
 
 app.handle('greeting', conv => {
  let message = 'A wondrous greeting, adventurer! Welcome back to the mythical land of Gryffinberg!';
- if (!conv.user.lastSeenTime) {
+ /*if (!conv.user.lastSeenTime) {
    message = 'Welcome to the mythical land of  Gryffinberg! Based on your clothes, you are not from around these lands. It looks like you\'re on your way to an epic journey.';
- }
+ }*/
  conv.add(message);
 });
 
 app.handle('unavailable_options', conv => {
-  const option = conv.intent.params.chosenUnavailableOption.original;
-  const optionKey = conv.intent.params.chosenUnavailableOption.resolved;
+  // const option = conv.intent.params.chosenUnavailableOption.original;
+  // const optionKey = conv.intent.params.chosenUnavailableOption.resolved;
   let message = 'I have seen the future and ';
-  if(optionsNeedA.has(optionKey)){
+ /* if(optionsNeedA.has(optionKey)){
     message = message + 'a ';
   }
-  message = message + `${option} will not aid you on your journey. `;
+  message = message + `${option} will not aid you on your journey. `;*/
   conv.add(message);
 });
 
