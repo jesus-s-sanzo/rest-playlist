66e72aa984110ba63485d54c654968777cfbacf5
Jesus <gsusss2004@gmail.com>
2021-03-29 13:37:39 +0100

    removed unused code

--- server.js
+++ server.js
@@ -3,7 +3,6 @@ const bodyParser = require('body-parser');
 
 const { conversation } = require('@assistant/conversation');
 const functions = require('firebase-functions');
-const admin = require('firebase-admin');
 
 const app = conversation({ debug: true });
 
@@ -20,7 +19,7 @@ app.handle('greeting', conv => {
 const expressApp = express().use(bodyParser.json());
 expressApp.use(express.static("public"));
 // serve the index.html file when visiting the homepage
-expressApp.get("/", function (request, response) {  response.sendFile(__dirname + "/index.html");});
+expressApp.get("/", function (request, response) { response.sendFile(__dirname + "/index.html"); });
 expressApp.get('/health', async function (req, res) { res.send({ "status": "working", "localtime": new Date() }); });
 
 expressApp.post('/', app);
@@ -32,20 +31,4 @@ module.exports = { 'expressApp': expressApp }
 expressApp.listen(port, () => console.log(`server listening on port ${port}`));
 
 module.exports.ActionsOnGoogleFulfillment = functions.https.onRequest(app);
-/*
-const optionsNeedA = new Set();
-optionsNeedA.add('horse').add('phone');
 
-app.intent('actions.intent.MAIN', conv => { conv.ask('Hi, how is it going MAIN camp?') });
-
-app.intent('actions.intent.TEXT', (conv, input) => {
-  if (input === 'bye' || input === 'goodbye') { return conv.close('See you later!') }
-  conv.ask(`I didn't understand. Can you tell me something else?`)
-});
-
-app.intent('actions.intent.greeting', conv => { conv.ask('A wondrous greeting, adventurer! Welcome back to the mythical land of Gryffinberg!'); })
-app.intent('unavailable_options', conv => { conv.ask('That wont help'); });
-
-app.intent('Welcome', (conv) => { console.log('welcome'); conv.ask('Welcome!'); });
-app.intent('greeting', (conv) => { console.log('greetng called'); conv.ask('geeting!'); });
-*/
